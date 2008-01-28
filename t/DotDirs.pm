740fef06cf2304926f07accd722c8dbcf5dc4d3a
David Golden <dagolden@cpan.org>
2008-01-28 15:30:51 +0000

    git-svn-id: https://dagolden.googlecode.com/svn/CPAN-Reporter-Smoker/trunk@1663 dfce27d5-b31c-0410-bb09-030b4413eeba

---
+++ t/DotDirs.pm
@@ -0,0 +1,39 @@
+package t::DotDirs;
+
+# stolen/adapted from local_utils.pm in the CPAN.pm distro
+
+use File::Path qw(rmtree mkpath);
+use File::Spec ();
+use IO::File;
+
+sub _f ($) {File::Spec->catfile(split /\//, shift);}
+sub _d ($) {File::Spec->catdir(split /\//, shift);}
+
+my $dot_cpan = _d"t/dot-cpan";
+my $dot_cpan_reporter = _d"t/dot-cpanreporter";
+
+sub prepare_cpan {
+    rmtree $dot_cpan if -d $dot_cpan;
+    mkpath $dot_cpan;
+    return $dot_cpan;
+}
+
+sub prepare_cpan_reporter {
+    rmtree $dot_cpan_reporter if -d $dot_cpan_reporter;
+    mkpath $dot_cpan_reporter;
+    my $config = IO::File->new( _f"$dot_cpan_reporter\/config.ini", ">" );
+    print {$config} <DATA>;
+    return $dot_cpan_reporter;
+}
+
+END { 
+    rmtree $dot_cpan;
+    rmtree $dot_cpan_reporter;
+}
+
+1;
+
+# standard .cpanreporter/config.ini for testing
+__DATA__
+email_from = johndoe@example.com
+command_timeout = 30
