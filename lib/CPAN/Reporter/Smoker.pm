55d8652a7345f58fdf865c061709200fc3f5b293
David Golden <dagolden@cpan.org>
2014-03-14 15:52:39 +0100

    fix typos

--- lib/CPAN/Reporter/Smoker.pm
+++ lib/CPAN/Reporter/Smoker.pm
@@ -577,7 +577,7 @@ smoke testing. This will reload indices to search for new distributions
 and restart testing from the most recent distribution. Must be a positive
 integer; Defaults to 43200 seconds (12 hours)
 * {skip_dev_versions} -- if true, unindexed distributions will not be tested.
-Valid values are 0 or 1. Defauilts to 0.
+Valid values are 0 or 1. Defaults to 0.
 * {set_term_title} -- toggle for whether the terminal titlebar will be
 updated with the distribution being smoke tested and the starting time
 of the test. Helps determine if a test is hung and which distribution
